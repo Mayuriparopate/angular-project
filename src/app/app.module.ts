636bae7ebf501fab876b66e39a1c6c2a84da5209
Mayuri Paropate <mayuri.paropate@kotak.com>
2023-08-17 12:22:37 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,40 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginDirective } from './directives/login.directive';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TempFormComponent } from './temp-form/temp-form.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TemDirectiveDirective } from './tem-directive.directive';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    LoginDirective,
+    ReactiveFormComponent,
+    TempFormComponent,
+    ProductListComponent,
+    TemDirectiveDirective,
+   
   ],
   imports: [
+   
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
